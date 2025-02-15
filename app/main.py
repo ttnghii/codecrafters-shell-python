550ccca009ccf53f8a095a1f8cab552f557f9a40
Tran Trieu Nghi <trantrieunghi@ShiKu.local>
2025-02-15 19:40:10 +0700

    moew moew

--- app/main.py
+++ app/main.py
@@ -16,9 +16,9 @@ def main():
             case command if command.startswith("type invalid"):
                 print(f"{command[len('type ') :]}: not found")
             case command if command.startswith("type valid"):
-                print(f"{command[len('type ') :]} is /usr/local/bin/{command[len('type ') :]}")
+                print(f"{command[len('type ') :]} is /local/bin/{command[len('type ') :]}")
             case command if command.startswith("type"):
-                print(f"{command[len('type ') :]} is bin/{command[len('type ') :]}")
+                print(f"{command[len('type ') :]} is /bin/{command[len('type ') :]}")
             case _:
                 print(f"{command}: command not found")
 
