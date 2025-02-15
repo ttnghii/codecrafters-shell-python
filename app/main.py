b22ff44ab12b6816213c78d66680da4ff6431746
Tran Trieu Nghi <trantrieunghi@ShiKu.local>
2025-02-15 17:45:36 +0700

    moew moew

--- app/main.py
+++ app/main.py
@@ -8,11 +8,12 @@ def main():
         sys.stdout.write("$ ")
         command = input().lstrip("echo ")
         
-        if "invalid" in command:
+        if command == "exit 0":
+            sys.exit(0)
+        elif "invalid" in command:
             print(f"{command}: command not found")
         else:   
             print(command)
-
 
 
 if __name__ == "__main__":
