0ff30748c89e944cb0bfaec4791e39df241d347f
Tran Trieu Nghi <trantrieunghi@ShiKu.local>
2025-02-15 19:17:07 +0700

    moew moew

--- app/main.py
+++ app/main.py
@@ -11,17 +11,17 @@ def main():
 
         if "invalid" in command:
             print(f"{" ".join(args[1:])}: not found")
-
-        new_command = " ".join(args[1:])
-        match args[0]:
-            case "type":
-                print(f"{new_command} is a shell builtin")
-            case "echo":
-                print(new_command)
-            case "exit":
-                sys.exit(0)
-            case default:
-                print(command)
+        else:
+            new_command = " ".join(args[1:])
+            match args[0]:
+                case "type":
+                    print(f"{new_command} is a shell builtin")
+                case "echo":
+                    print(new_command)
+                case "exit":
+                    sys.exit(0)
+                case default:
+                    print(command)
 
 
 if __name__ == "__main__":
